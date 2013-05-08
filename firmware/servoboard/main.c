afba85648d1ff9a344bed28e73c5c20002d11bcb
rainer <rainer@wwad.de>
2013-05-08 12:28:06 +0200

    servoboard: firmware nahezu fertig

--- firmware/servoboard/main.c
+++ firmware/servoboard/main.c
@@ -30,9 +30,13 @@
 
 #define RECVcommand 0
 #define RECVangular 1
+#define RECVangular2 2
 
 #define CMD_SERVO 0
 #define CMD_LED 1
+
+#define MILLISECONDBASE 8000
+
 
 uint8_t twi_activity;
 uint8_t twi_dataactivity;
@@ -46,8 +50,16 @@ uint8_t twistate = TWISTATE_standby;
 uint8_t recvstate = RECVcommand;
 uint8_t data_complete;
 
+uint8_t angularh;
 uint8_t servo_waiting_for_data;
-uint8_t servos_angular[8];
+
+uint8_t led_controlled_by_user = 0;
+
+uint16_t servos_angular[8];
+uint8_t * servoports[8+1]; //+1 for dummy servo
+uint8_t servopins[8+1]; //+1 for dummy servo
+uint8_t current_servo = 7;
+uint16_t pulsetime = 0;
 
 void buffer_write(uint8_t data){ //if executed on full buffer, data will be dropped
   uint8_t nw_new = (twi_buffer_nw+1)%TWIBUFFERSIZE;
@@ -66,6 +78,51 @@ uint8_t buffer_read(){ //should NOT BE CALLED IF BUFFER IS EMPTY!!!
     twi_buffer_empty = 1; //we will read the last emelent in buffer now!
   }
   return twi_data_buffer[nr_old];
+}
+
+void servo_init(){
+  //configure mapping of servos to pins:
+  //FIXME: this could be made static constants
+  servoports[0] = &PORTB;
+  servopins[0] = PB1;
+
+  servoports[1] = &PORTB;
+  servopins[1] = PB2;
+
+  servoports[2] = &PORTC;
+  servopins[2] = PC0;
+
+  servoports[3] = &PORTC;
+  servopins[3] = PC1;
+
+  servoports[4] = &PORTC;
+  servopins[4] = PC2;
+
+  servoports[5] = &PORTC;
+  servopins[5] = PC3;
+
+  servoports[6] = &PORTD;
+  servopins[6] = PD1;
+
+  servoports[7] = &PORTD;
+  servopins[7] = PD1;
+
+  //dummy servo:
+  servoports[8] = &PORTD;
+  servopins[8] = PD1;
+
+  //all servos to middle position, pulse disabled
+  uint8_t i;
+  for(i = 0; i<8; i++){
+    servos_angular[i] = 4000;
+    *servoports[i] &= ~(1<<servopins[i]);
+  }
+  current_servo = 8; //start with dummy servo... next one will be servo0 
+  pulsetime = 20000; //simulate, that the previous pulse was complete
+
+  TCNT1 = 0; //reset timer
+  OCR1A = 4000; //next pulse will start in 1ms
+  TCCR1B = (1<<CS10); //prescaler 1 start timer
 }
 
 void port_init(){
@@ -124,9 +181,11 @@ void twi_handle(){
               if(data_complete&(1<<4)==0){ //led ausschalten
                 switch(data_complete<<5){
                   case 0:
+                    led_controlled_by_user = 1;
                     led1_aus;
                     break;
                   case 1:
+                    led_controlled_by_user = 2;
                     led2_aus;
                     break;
                   case 2:
@@ -136,9 +195,11 @@ void twi_handle(){
               }else{
                 switch(data_complete<<5){
                   case 0:
+                    led_controlled_by_user = 1;
                     led1_an;
                     break;
                   case 1:
+                    led_controlled_by_user = 2;
                     led2_an;
                     break;
                   case 2:
@@ -150,9 +211,16 @@ void twi_handle(){
           }
           break;
         case RECVangular:
-          servos_angular[servo_waiting_for_data] = data_complete;
+          //receive the msbs of angular
+          angularh = data_complete;
+          //servos_angular[servo_waiting_for_data] = data_complete;
 
-          recvstate = RECVcommand; //next data bit will be a command
+          recvstate = RECVangular2; //next data byte will be the lsbs of angular
+          break;
+        case RECVangular2:
+          servos_angular[servo_waiting_for_data] = (((uint16_t)angularh)<<8) | (uint16_t)data_complete;
+
+          recvstate = RECVcommand; //next data byte will be a command
           twistate = TWISTATE_standby; //goback to standby mode awaiting new transmission
           break;
       }
@@ -161,13 +229,64 @@ void twi_handle(){
   }
 }
 
-ISR(TWI_vect){ //TWI interrupt handler
+ISR(TWI_vect){ //TWI interrupt handler //FIXME: what happens if i can not read data fast enough?
   uint8_t status = TWSR;
   twi_activity = 1;
   if(status == 0x80 || status == 0x90){ //daten empfangen an eigene adresse oder an general address
     twi_dataactivity = 1;
     buffer_write(TWDR); //daten in buffer schreiben
   }
+}
+
+
+//used for clearing leds
+ISR(TIMER0_OVF_vect){ //32ms at 8MHz
+  switch (led_controlled_by_user){
+    case 0:
+      led1_aus;
+      led2_aus;
+      break;
+    case 1:
+      led2_aus;
+      break;
+    case 2:
+      led1_aus;
+      break;
+  }
+}
+
+ISR(TIMER1_COMPA_vect){
+  //aktuellen servopuls beenden
+  *servoports[current_servo] &= ~(1<<servopins[current_servo]);
+
+  //stop and reset timer:
+  TCCR1B = 0; //stop
+  TCNT1=0;    //reset
+
+  //configure for next servo
+  current_servo = (current_servo + 1) % (8+1); //one dummy servo for 20ms correction
+
+  if(current_servo!=8){ //real servo
+    sei();//disable interrupts //FIXME: can interrupts happen here at all?
+    OCR1A = MILLISECONDBASE + servos_angular[current_servo];
+    cli();//re enable interrupts
+    pulsetime+=OCR1A/8;
+    
+    //start pulse of next servo:
+    *servoports[current_servo] |= (1<<servopins[current_servo]);
+  }else{ //dummy servo will fill up time to 20ms
+      uint16_t timeremaining = (20000-pulsetime);
+      if(timeremaining<8192){ //dummy servo can compensate remaining time
+        OCR1A = timeremaining*8;
+        pulsetime = 0;
+      }else{
+        OCR1A = 0xffff;
+        pulsetime+=8191;
+        current_servo = 7; //next time we will have a dummy servo again :)
+      }
+  }
+  //start timer
+  TCCR1B = (1<<CS10); //prescaler 1
 }
 
 void pwm_init(void) {
@@ -179,19 +298,43 @@ void pwm_init(void) {
 	TCCR1B = (1<<CS10) | (1<<WGM13) | (1<<WGM12);
 }
 
+void timer_init(){
+  
+  //enable interrupt for timer0 (8bit)
+  //used for led clearance
+  TIMSK |= (1<<TOIE0);
+
+  //enable compare match (A) interrupt for Timer1 (16bit)
+  TIMSK |= (1<<OCIE1A);
+}
+
 int main (void)
 {
   port_init();
+  timer_init;
   i2cinit();
-
+  
+  servo_init();
   sei();  //enable global interrupts
 	while (1) { 
-    if(twi_activity == 1){
+  
+    //try to read data from twi buffer:
+    twi_handle();
+
+    //handle status leds
+    if(twi_activity == 1 && led_controlled_by_user!=1){
       led1_an;
+      //configure timer0 to clear the leds for us
+      TCNT0 = 0;
+      TCCR0 = (1<<CS02) | (1<<CS00); //8bit timer0 prescaler to 1024
     }
-    if(twi_dataactivity == 1){
+    if(twi_dataactivity == 1 && led_controlled_by_user!=2){
       led2_an;
+      //configure timer0 to clear the leds for us
+      TCNT0 = 0;
+      TCCR0 = (1<<CS02) | (1<<CS00); //8bit timer0 prescaler to 1024
     }
-    _delay_ms(20);
+
+
 	}
 }

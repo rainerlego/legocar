b2b3603d2f6ea407ac638b948e07d2c2da5157e7
rainerlaptop <rainer@wwad.de>
2013-05-16 10:18:47 +0200

    testservp.py angepasst mit cli arg

--- firmware/servoboard/testservo.py
+++ firmware/servoboard/testservo.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import sys
 
 s = serial.Serial(port="/dev/ttyUSB0", baudrate=115200, timeout=0.1, writeTimeout=0.1)
 s.write("#\n")
@@ -10,15 +11,16 @@ s.write("1\n")
 s.flush()
 print s.read(1024)
 
-for i in range(0,65500, 100):
-    print(str(i) + "\n" )
-    s.write("[")
-    s.write("0x02,")
-    s.write("0xff,")
-    s.write("0x00,")
-    s.write(str(i / 256))
-    s.write(",")
-    s.write(str(i % 256))
-    s.write("]\r\n")
-    s.flush()
-    print s.read(1024)
+i = int(sys.argv[1]);
+
+print(str(i) + "\n" )
+s.write("[")
+s.write("0x02,")
+s.write("0xff,")
+s.write("0x01,")
+s.write(str(i / 256))
+s.write(",")
+s.write(str(i % 256))
+s.write("]\r\n")
+s.flush()
+print s.read(1024)

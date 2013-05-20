3376153c46fc901aa8c5093c64216a0894f13a26
Christian Dietz <christian@wwad.de>
2013-05-20 16:20:26 +0200

    sim: tcpserver

--- sim/main.cc
+++ sim/main.cc
@@ -2,17 +2,35 @@
 #include <gtkmm/application.h>
 #include <gtkmm/window.h>
 
+#include "config.h"
+#include "tcpserver.h"
+
+void *tcp_thread(void * v)
+{
+  struct tcpserver ts;
+  ts.port = SERVER_PORT;
+  tcpserver_start ( &ts );
+}
+
 int main(int argc, char** argv)
 {
-   Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
-   Gtk::Window win;
-   win.set_title("DrawingArea");
+  Gtk::Window win;
+  win.set_title("DrawingArea");
 
-   MyArea area;
-   win.add(area);
-   area.show();
+  pthread_t tcpthread;
 
-   return app->run(win);
+  MyArea area;
+  win.add(area);
+  area.show();
+
+  if( pthread_create( &tcpthread , NULL ,  tcp_thread , NULL) < 0)
+  {
+   perror("E: main: could not create tcp thread\n");
+   return -1;
+  }
+
+  return app->run(win);
 }
 

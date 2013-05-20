4f97782981c5ae889f902b6f17300eafe45aa10a
Christian Dietz <christian@wwad.de>
2013-05-20 19:12:13 +0200

    compile-scripts brauchen wir nicht mehr

--- sim/compile.sh
+++ sim/compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-c++ `pkg-config --cflags --libs gtkmm-3.0` -lm -lpthread main.cc global.c tcpserver.c car.cc camera.cc myarea.cc vectors.cc map.cc sensor.cc
+#c++ `pkg-config --cflags --libs gtkmm-3.0` -lm -lpthread main.cc global.c tcpserver.c car.cc camera.cc myarea.cc vectors.cc map.cc sensor.cc
+make -j$1

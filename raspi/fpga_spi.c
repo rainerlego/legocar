b99c73cf14f6116f370a4e813b97834a96510060
Christian Dietz <christian@wwad.de>
2013-07-10 12:30:28 +0200

    raspi: spi zeuch

--- raspi/fpga_spi.c
+++ raspi/fpga_spi.c
@@ -11,64 +11,66 @@
 
 int fd;
 
+static uint8_t spi_mode,spi_bits;
+static uint32_t spi_speed;
+static uint16_t spi_delay;
+
 int fpga_open()
 {
-    int ret;
-	printf ( "Will use SPI to send commands via %s\n", FPGA_SPI_DEV );
+  int ret;
+  printf ( "Will use SPI to send commands to fpga\n" );
+  printf("spi configuration: \n" );
+  printf("    + dev: %s\n", FPGA_SPI_DEV );
+  printf("    + mode: %d\n", spi_mode);
+  printf("    + bits per word: %d\n", spi_bits);
+  printf("    + speed: %d Hz (%d KHz)\n", spi_speed, spi_speed/1000);
+  printf("\n");
+
+  spi_mode = SPI_MODE;
+  spi_bits = SPI_BITS_PER_WORD;
+  spi_speed = SPI_SPEED_HZ;
+  spi_delay = SPI_DELAY;
 
   if ((fd = open(FPGA_SPI_DEV, O_RDWR)) < 0) {
-    printf("E: fpga: Failed to open printf port\n");
+    printf("E: fpga: spi: Failed to open dev\n");
     return -1;
   }
   
-	/*
-	 * spi mode
-	 */
-	ret = ioctl(fd, SPI_IOC_WR_MODE, SPI_MODE);
-	if (ret == -1){
-		printf("can't set spi mode");
-        return -1;
-    }
+  if ((ret = ioctl(fd, SPI_IOC_WR_MODE, &spi_mode)) < 0 )
+  {
+    printf("E: fpga: spi: can't set spi mode wr\n");
+    return -1;
+  }
     
-	ret = ioctl(fd, SPI_IOC_RD_MODE, SPI_MODE);
-	if (ret == -1){
-		printf("can't get spi mode");
-        return -1;
-    }
+  if ((ret = ioctl(fd, SPI_IOC_RD_MODE, &spi_mode)) < 0 )
+  {
+    printf("E: fpga: spi: can't set spi mode rd\n");
+    return -1;
+  }
     
-	/*
-	 * bits per word
-	 */
-	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, SPI_BITS_PER_WORD);
-	if (ret == -1){
-		printf("can't set bits per word");
-        return -1;
-    }
+  if ((ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &spi_bits)) < 0 )
+  {
+    printf("E: fpga: spi: can't set bits per word wr\n");
+    return -1;
+  }
     
-	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, SPI_BITS_PER_WORD);
-	if (ret == -1){
-		printf("can't get bits per word");
-        return -1;
-    }
+  if ((ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &spi_bits)) < 0 )
+  {
+    printf("E: fpga: spi: can't set bits per word rd\n");
+    return -1;
+  }
     
-	/*
-	 * max speed hz
-	 */
-	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, SPI_SPEED_HZ);
-	if (ret == -1){
-		printf("can't set max speed hz");
-        return -1;
-    }
+  if ((ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &spi_speed)) < 0 )
+  {
+    printf("E: fpga: spi: can't set speed wr\n");
+    return -1;
+  }
     
-	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, SPI_SPEED_HZ);
-	if (ret == -1){
-		printf("can't get max speed hz");
-        return -1;
-    }
-    
-	printf("spi mode: %d\n", SPI_MODE);
-	printf("bits per word: %d\n", SPI_BITS_PER_WORD);
-	printf("max speed: %d Hz (%d KHz)\n", SPI_SPEED_HZ, SPI_SPEED_HZ/1000);
+  if ((ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &spi_speed)) < 0 )
+  {
+    printf("E: fpga: spi: can't set speed rd\n");
+    return -1;
+  }
 
   return 0;
 }
@@ -77,8 +79,6 @@ void fpga_close()
 {
   close(fd);
 }
-
-
 
 int fpga_setservo ( uint8_t servoNr, uint16_t servoPos )
 {
@@ -138,29 +138,29 @@ int fpga_setspeedv (uint16_t vspeed, uint16_t vsteering )
 int spisend ( char*rbuf, char*wbuf, int len )
 {
 
-	int ret;
+  int ret;
 
-	struct spi_ioc_transfer tr = {
-		.tx_buf = (unsigned long)wbuf,
-		.rx_buf = (unsigned long)rbuf,
-		.len = len,
-		.delay_usecs = SPI_DELAY,
-		.speed_hz = SPI_SPEED_HZ,
-		.bits_per_word = SPI_BITS_PER_WORD,
-	};
+  struct spi_ioc_transfer tr = {
+    .tx_buf = (unsigned long)wbuf,
+    .rx_buf = (unsigned long)rbuf,
+    .len = len,
+    .delay_usecs = SPI_DELAY,
+    .speed_hz = SPI_SPEED_HZ,
+    .bits_per_word = SPI_BITS_PER_WORD,
+  };
     
-	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-	if (ret < 1){
-		printf("can't send spi message");
+  ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+  if (ret < 1){
+    printf("can't send spi message");
         return -1;
     }
     
-	for (ret = 0; ret < len; ret++) {
-		if (!(ret % 6))
-			puts("");
-		printf("%.2X ", wbuf[ret]);
-	}
-	puts("");
+  for (ret = 0; ret < len; ret++) {
+    if (!(ret % 6))
+      puts("");
+    printf("%.2X ", wbuf[ret]);
+  }
+  puts("");
 }
 
 void fpga_testservos()
@@ -188,6 +188,6 @@ void fpga_testservos()
 int fpgamain(int argc, char *argv[])
 {
     testservos();
-	return 0;
+  return 0;
 }
 */
